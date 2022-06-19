d5bf843a3cf4c9443f9f3d6022b972437528294c
wyr <1950636744@qq.com>
2022-06-19 12:38:18 +0800

    push commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git3!");
+        System.out.println("push git!");
     }
 }
